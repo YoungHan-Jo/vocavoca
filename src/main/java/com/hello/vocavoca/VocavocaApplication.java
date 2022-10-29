af1d5dbee871a62cedf8e062e2b09bb456dfb8bc
Jo YoungHan <verin4494@gmail.com>
2022-10-29 13:54:21 +0900

    setting

---
+++ src/main/java/com/hello/vocavoca/VocavocaApplication.java
@@ -0,0 +1,13 @@
+package com.hello.vocavoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocavocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocavocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hello/vocavoca/VocavocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.vocavoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocavocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
